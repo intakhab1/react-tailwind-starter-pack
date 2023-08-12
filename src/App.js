387c20f816a24b35a23eea37c8b0ab7579b8b32c
intakhab1 <amirint190@gmail.com>
2023-08-12 16:15:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main class="flex justify-center gap-4 flex-col min-h-screen">
+      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
+      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
+    </main>
+  );
+}
+
+export default App;
